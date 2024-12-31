2cda54883c117072299d1ff63036f334453994b6
= <thiagoespindula00@gmail.com>
2024-12-31 11:57:00 -0300

    criado identificador único para a entidade Evento

--- src/main/java/br/com/alura/codechella/domain/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/domain/evento/Evento.java
@@ -5,13 +5,28 @@ import br.com.alura.codechella.domain.ingresso.TipoIngresso;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Evento {
+    private UUID uuid;
     private Categoria categoria;
     private String descricao;
     private LocalDateTime data;
     private Endereco endereco;
     private List<TipoIngresso> tiposIngresso = new ArrayList<>();
+
+    public Evento(Categoria categoria, String descricao, LocalDateTime data, Endereco endereco) {
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.data = data;
+        this.endereco = endereco;
+
+        gerarUUID();
+    }
+
+    private void gerarUUID() {
+        this.uuid = UUID.randomUUID();
+    }
 
     public void adicionaTipoIngresso(TipoIngresso tipoIngresso) {
         this.tiposIngresso.add(tipoIngresso);
@@ -19,6 +34,10 @@ public class Evento {
 
     public void aumentaNumeroDeIngressosPorTipo() {
 
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 
     public Categoria getCategoria() {
